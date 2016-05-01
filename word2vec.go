e5a7a9a67f1767bd5ac7dcb0bfe555ec72c9ea9d
Gaurav Menghani <gauravm@fb.com>
2016-05-01 16:09:36 -0700

    Sort the vocabulary and print the top few words

--- word2vec.go
+++ word2vec.go
@@ -8,6 +8,7 @@ import (
   "fmt"
   // "map"
   "os"
+  "sort"
 )
 
 type trainParams struct {
@@ -23,6 +24,25 @@ func normalizeChar(b byte) byte {
     return 'a' + (b - 'A')
   }
   return b
+}
+
+type vocabWord struct {
+  word string
+  count int
+}
+
+type vocabulary []vocabWord
+
+func (vocab vocabulary) Len() int {
+  return len(vocab)
+}
+
+func (vocab vocabulary) Less(i, j int) bool {
+  return vocab[i].count > vocab[j].count
+}
+
+func (vocab vocabulary) Swap(i, j int) {
+  vocab[i], vocab[j] = vocab[j], vocab[i]
 }
 
 func readFile(params trainParams) {
@@ -76,6 +96,18 @@ func readFile(params trainParams) {
   fmt.Printf("\n")
   fmt.Println("Error ", e)
   fmt.Printf("Total words = %d, Unique words = %d\n", totalWords, len(freqCount))
+
+  vocab := make(vocabulary, 0)
+  for word, count := range freqCount {
+    vocab = append(vocab, vocabWord{word, count})
+  }
+
+  sort.Sort(vocab)
+
+  for i := 0; i < 100; i++ {
+    fmt.Printf("Top Word: %s, Index: %d Count: %d\n",
+      vocab[i].word, i, vocab[i].count)
+  }
 }
 
 func main() {
